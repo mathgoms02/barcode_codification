0425cef42fba5126962b43bdf263ab24f0f14f0c
mathgoms02 <math.gomsx3@gmail.com>
2024-02-12 20:20:39 -0300

    Gerando vários códigos de barras de uma vez

--- barcode_codification.py
+++ barcode_codification.py
@@ -2,12 +2,28 @@ from barcode import EAN13
 from barcode.writer import ImageWriter
 import random
 
-chaves_acesso = {
-    "Matheus Gomes_54701": str(random.randint(111111111111, 999999999999)),
-    "Felipe Moreira_3001": str(random.randint(111111111111, 999999999999)),
-    "Ricardo_Barbosa_6549": str(random.randint(111111111111, 999999999999))
-}
+chaves_acesso = []
 
-for pessoas in chaves_acesso:
-    codigo_barra = EAN13(chaves_acesso[pessoas], writer=ImageWriter())
-    codigo_barra.save(f'{pessoas}_key')
+print("PARA SAIR, DIGITE 'sair'\n")
+
+def inser(name, re):
+    acess_key = {f"{name}_{re}" : str(random.randint(111111111111, 999999999999))}
+    print(acess_key,"\n")
+    return acess_key
+
+name = ""
+
+while name != "sair":
+    name = input("Nome: ")
+    if name == "sair":
+        break
+    re = input("RE: ")
+    if re == "sair":
+        break
+    chaves_acesso.append(inser(name, re))
+
+if chaves_acesso:
+    for pessoas in chaves_acesso:
+        for nome, chave in pessoas.items():
+            codigo_barra = EAN13(chave, writer=ImageWriter())
+            codigo_barra.save(f'barcodes/{nome}_key')
